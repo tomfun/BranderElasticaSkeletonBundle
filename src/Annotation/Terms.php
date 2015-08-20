e72599a77db347e674f9e0a3699e7f3c3a2b0d31
tomfun <tomfun1990@gmail.com>
2015-08-21 01:38:00 +0300

    comment

--- src/Annotation/Terms.php
+++ src/Annotation/Terms.php
@@ -2,10 +2,13 @@
 namespace Brander\Bundle\ElasticaSkeletonBundle\Annotation;
 
 /**
+ * Query to find something in array. Something can be also array. Look at links bellow.
  * @class Terms
  * @author Tomfun <tomfun1990@gmail.com>
  * @Annotation
  * @Target({"METHOD", "PROPERTY"})
+ * @link http://stackoverflow.com/questions/21933787/elasticsearch-not-returning-results-for-terms-query-against-string-property
+ * @link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-terms-query.html
  */
 final class Terms extends Query
 {
